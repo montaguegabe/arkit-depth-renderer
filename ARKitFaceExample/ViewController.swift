bf3cc05d52499fa1c2a33613fd14704cdbfacf32
Gabriel Montague <gmontague@college.harvard.edu>
2018-01-02 18:23:35 -0500

    Working

--- ARKitFaceExample/ViewController.swift
+++ ARKitFaceExample/ViewController.swift
@@ -38,7 +38,15 @@ class ViewController: UIViewController, ARSessionDelegate {
             contentUpdater.virtualFaceNode = nodeForContentType[selectedVirtualContent]
         }
     }
-
+    
+    var context = CIContext()
+    let backgroundTransform = SCNMatrix4Scale(SCNMatrix4MakeRotation(Float.pi / 2.0, 0, 0, 1), -1, 1, 1)
+    
+    var num : CGFloat = 0
+    
+    // Woah this line of code is incredibly cool. If it doesn't work then your Xcode is probably not up to date.
+    let gradientImage = CIImage(cgImage: #imageLiteral(resourceName: "Gradient.png").cgImage!)
+    
     // MARK: - View Controller Life Cycle
 
     override func viewDidLoad() {
@@ -47,6 +55,9 @@ class ViewController: UIViewController, ARSessionDelegate {
         sceneView.delegate = contentUpdater
         sceneView.session.delegate = self
         sceneView.automaticallyUpdatesLighting = true
+        
+        // Display black background until depth is available
+        sceneView.scene.background.contents = UIColor.black
         
         createFaceGeometry()
 
@@ -94,7 +105,45 @@ class ViewController: UIViewController, ARSessionDelegate {
     }
     
     // MARK: - ARSessionDelegate
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        
+        let depthData = frame.capturedDepthData
+        if depthData != nil {
+            
+            // Render depth data to background texture (not quite as fast as using Metal textures)
+            let depthBuffer : CVPixelBuffer = depthData!.depthDataMap
+            
+            let width = CVPixelBufferGetWidth(depthBuffer)
+            let height = CVPixelBufferGetHeight(depthBuffer)
+            let frameWidth = sceneView.frame.size.width
+            let frameHeight = sceneView.frame.size.height
+            let depthImage = CIImage(cvImageBuffer: depthBuffer)
+            let targetSize = CGSize(width: frameWidth, height: frameHeight)
+            
 
+            var normalizeTransform = CGAffineTransform(a: 1.0 / CGFloat(width), b: 0, c: 0, d: -1.0 / CGFloat(height), tx: 0, ty: 1.0)
+            var correctTransform = frame.displayTransform(for: UIInterfaceOrientation.portrait, viewportSize: targetSize) //.inverted()
+            var rescaleTransform = CGAffineTransform(a: CGFloat(frameWidth), b: 0, c: 0, d: CGFloat(frameHeight), tx: 0, ty: 0)
+            var transform = normalizeTransform.concatenating(correctTransform).concatenating(rescaleTransform)
+            let depthImageTransformed = depthImage.transformed(by: transform)
+            
+            // Do color map
+            let filter = CIFilter(name: "CIColorMap")!
+            filter.setValue(gradientImage, forKey: kCIInputGradientImageKey)
+            filter.setValue(depthImageTransformed, forKey: kCIInputImageKey)
+            let mappedImage = filter.outputImage!
+            
+
+            let renderedBackground : CGImage = context.createCGImage(mappedImage, from: CGRect(x: 0, y: 0, width: CGFloat(frameWidth), height: CGFloat(frameHeight)))!
+            let texture = SKTexture(cgImage: renderedBackground)
+            
+            sceneView.scene.background.contents = texture
+            sceneView.scene.background.contentsTransform = SCNMatrix4Identity
+            
+        }
+    }
+    
     func session(_ session: ARSession, didFailWithError error: Error) {
         guard error is ARError else { return }
         
@@ -136,7 +185,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         configuration.isLightEstimationEnabled = true
         session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
-
+    
     // MARK: - Interface Actions
 
     /// - Tag: restartExperience
